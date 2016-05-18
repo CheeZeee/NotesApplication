e11a75e9ca53ceb9fb43b047923066425b39a9d9
Chika Nathan <Chika Nathan>
2016-05-18 08:58:26 +0100

    Create a function that adds a note to the list

--- NotesApplication.js
+++ NotesApplication.js
@@ -1,3 +1,4 @@
+ // This program is intended to implement a note taking application
  function NotesApplication(author){
 	this.author = author;
 	this.notes = [];
@@ -5,49 +6,10 @@
 	// This function takes the note content as the parameter 
 	// and adds it to the notes list of the object.
 	NotesApplication.prototype.create = function(note_content){
-		if(typeOf.note_content == typeOf("abc")) {
+		if(typeof note_content) === 'string' {
 			this.notes.push(note_content);
-	}
-
-	// This function lists out each of the notes in the notes list
-	NotesApplication.prototype.listNotes = function listNotes(notes){
-	
-	}
-
-	function get(note_id){
-
-	}
-	// This function take a search string
-	// and returns all the notes with that text within it
-	NotesApplication.prototype.search = function(search_text){
-		if (typeOf(search_text) = typeOf("abc")){
-			for (i = 0, i < this.notes.length, i++){
-				if (this.note[i].indexOf(search_text) != 0){
-					Console.log("Showing results for search " + search_text)
-					Console.log("NOTE ID: " + id)
-					Console.log(note_content)
-					Console.log("By Author " + author)
-
-				}
-				else{
-					return "Search item should be of the string format"
-				}
-			}
-		}
-	}
-	// This function deletes the note at the index of the notes list
-	NotesApplication.prototype.delete = function(note_id){
-		if(typeOf(note_content) == typeOf(0)){
-			this.note.splice(note_content)
 		}
 		else{
-			return "This note does not exist"
+			return "Content should be of the string type"
 		}
 	}
-	// This function replaces the content in the note at note_id with new_content.
-	NotesApplication.prototype.edit = function(note_id, new_content){
-		if ()
-
-	}
-
-}
